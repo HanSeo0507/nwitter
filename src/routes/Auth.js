542f62a084559a044ecb849a77e5edb07d5f959a
HanSeo <khan3618@naver.com>
2020-09-16 08:36:09 +0900

    2.4 Login

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -5,6 +5,7 @@ const Auth = () => {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 	const [newAccount, setNewAccount] = useState(true);
+	const [error, setError] = useState("");
 
 	const onChange = (event) => {
 		const {
@@ -29,9 +30,11 @@ const Auth = () => {
 			}
 			console.log(data);
 		} catch (error) {
-			console.log(error);
+			setError(error.message);
 		}
 	};
+
+	const toggleAccount = () => setNewAccount((prev) => !prev);
 
 	return (
 		<div>
@@ -39,8 +42,9 @@ const Auth = () => {
 				<input name="email" type="email" placeholder="Email" required value={email} onChange={onChange} />
 				<input name="password" type="password" placeholder="Password" required value={password} onChange={onChange} />
 				<input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+				{error}
 			</form>
-
+			<span onClick={toggleAccount}>{newAccount ? "Log In" : "Create Account"}</span>
 			<div>
 				<button>Continue With Google</button>
 				<button>Continue With Github</button>
