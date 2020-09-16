8e8d70822bce76ee2d11ee648cc29cd843ab26c5
HanSeo <khan3618@naver.com>
2020-09-17 00:19:38 +0900

    4.0 Preview Images part One

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -37,11 +37,24 @@ const Home = ({ userObj }) => {
 		setNweet(value);
 	};
 
+	const onFileChange = (event) => {
+		const {
+			target: { files },
+		} = event;
+
+		const theFile = files[0];
+		const reader = new FileReader();
+		reader.onload = (finishedEvent) => {
+			console.log(finishedEvent);
+		};
+		reader.readAsDataURL(theFile);
+	};
+
 	return (
 		<div>
 			<form onSubmit={onSubmit}>
 				<input value={nweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
-				<input type="file" accept="image/*" />
+				<input type="file" accept="image/*" onChange={onFileChange} />
 				<input type="submit" value="Nweet" />
 			</form>
 			<div>
